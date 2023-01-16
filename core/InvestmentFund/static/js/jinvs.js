982283bf2f069ba2ff92ebe17e69eadb5c9c5e86
Simon Velasquez <savelasquezo@gmail.com>
2023-01-16 17:11:44 -0500

    Nuevas Paginas-Edicion V2

--- core/InvestmentFund/static/js/jinvs.js
+++ core/InvestmentFund/static/js/jinvs.js
@@ -1,6 +1,9 @@
 const cashInput = document.getElementById('cash');
 const ctotal = document.getElementById('ctotal');
 
+const rammount = document.getElementById('rammount');
+const rinterest = document.getElementById('rinterest');
+const rtotal = document.getElementById('rtotal');
 
 const choices = document.getElementsByClassName('button-choices');
 
@@ -15,6 +18,12 @@ for (let i = 0; i < choices.length; i++) {
         const interest = e.target.value.split("-")[1].replace("%","")/100;
         const months = e.target.value.split("-")[0].replace(" Meses","");
         const total = Math.floor(cashValue*(1+(interest*months)));
-        ctotal.textContent = `${total.toLocaleString()}`;
+
+        const finterest = Math.floor(total-cashValue);
+
+        ctotal.textContent = `$ ${total.toLocaleString()}`;
+        rammount.textContent = `$ ${cashValue.toLocaleString()}`;
+        rinterest.textContent = `$ ${finterest.toLocaleString()}`;
+        rtotal.textContent = `$ ${total.toLocaleString()}`;
     });
 }
