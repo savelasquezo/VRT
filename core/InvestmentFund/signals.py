f759e3cdb0a2ab46be68b75fbe6935de3ae05ffc
Simon Velasquez <savelasquezo@gmail.com>
2023-01-31 09:18:28 -0500

    Actualizacion

--- core/InvestmentFund/signals.py
+++ core/InvestmentFund/signals.py
@@ -11,25 +11,30 @@ def add_record(sender, instance, **kwargs):
     if instance.rState == "Aprobado":
 
         CUser = Usuario.objects.filter(username=instance.username)
-        
-        CUser.update(
-            ref_id = instance.staff_cod,
-            ref_name = instance.staff,
-            ref_interest = 1,
-            is_active = True,
-            is_operating =True,
-            full_name = instance.full_name,
-            email = instance.email,
-            phone = instance.phone,
-            country = instance.country,
-            codigo = instance.codigo,
-            bank = instance.bank,
-            bank_account = instance.bank_account,
-            ammount = instance.ammount,
-            interest = instance.interest,
-            date_joined = timezone.now(),
-            date_expire = instance.date_expire
-            )
+
+        try:
+            CUser.update(
+                ref_id = instance.staff_cod,
+                ref_name = instance.staff,
+                ref_interest = 1,
+                is_active = True,
+                is_operating =True,
+                full_name = instance.full_name,
+                email = instance.email,
+                phone = instance.phone,
+                country = instance.country,
+                codigo = instance.codigo,
+                bank = instance.bank,
+                bank_account = instance.bank_account,
+                ammount = instance.ammount,
+                interest = instance.interest,
+                date_joined = timezone.now(),
+                date_expire = instance.date_expire
+                )
+        except Exception as e:
+            with open("/home/savelasquezo/apps/vrt/core/logs/signal_err.txt", "a") as f:
+                f.write("SignalError: {}\n".format(str(e)))
+
         
         
 @receiver(post_save, sender=Tickets)
