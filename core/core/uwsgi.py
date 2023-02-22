4e79a8a6f4eb70a05cf9a23156e61b60095249db
Simon Velasquez <savelasquezo@gmail.com>
2023-02-22 15:54:05 -0500

    UWSG MediaConfig V2

--- core/core/uwsgi.py
+++ core/core/uwsgi.py
@@ -1,10 +1,11 @@
 from .settings  import *
 
 DEBUG = False
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 ALLOWED_HOSTS = ["167.71.28.44","vrtfund.com","www.vrtfund.com"]
 
 CSRF_TRUSTED_ORIGINS = ['https://vrtfund.com','https://www.vrtfund.com']
 
-MEDIA_ROOT = '/home/savelasquezo/apps/vrt/core'
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '')
+MEDIA_URL = '/'
