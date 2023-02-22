735135d7e0b1704bddcf057fc642b7fa615b111d
Simon Velasquez <savelasquezo@gmail.com>
2023-02-22 15:44:34 -0500

    UWSG MediaConfig

--- core/core/uwsgi.py
+++ core/core/uwsgi.py
@@ -5,3 +5,6 @@ DEBUG = False
 ALLOWED_HOSTS = ["167.71.28.44","vrtfund.com","www.vrtfund.com"]
 
 CSRF_TRUSTED_ORIGINS = ['https://vrtfund.com','https://www.vrtfund.com']
+
+MEDIA_ROOT = '/home/savelasquezo/apps/vrt/core'
+MEDIA_URL = '/media/'
